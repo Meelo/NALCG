48605a99222cf8485074aad6bc1936b4e5e9f521
Niko Hämäläinen <milo@meelo.org>
2010-03-08 23:08:28 +0200

    updated use-case model. eating is now a base action, which always includes a move-action, checking ectends move-action and checkmate extends check.

--- tools/autotest.sh
+++ tools/autotest.sh
@@ -16,3 +16,22 @@ MAKEOPTS="-j5"
 make_cmd="make $MAKEOPTS"
 make_quiet="make $MAKEOPTS 1>/dev/null"
 make_extra_quiet="make $MAKEOPTS 2>/dev/null 1>/dev/null"
+
+function usage {
+    declare -A instructions
+    instructions["--quiet"]="Suppresses all standard output from tests."
+    instructions["--extra-quiet"]="Suppresses all standard and error output from tests."
+
+    for key in "${!instructions[@]}"
+    do
+        printf "%-20s%-59s\n" "$key" "${instructions[$key]}"
+    done
+}
+
+if [[ "$1" == "--quiet" ]]; then 
+    make_cmd="$make_quiet"
+elif [[ "$1" == "--extra-quiet" ]]; then
+    make_cmd="$make_extra_quiet"
+fi
+
+usage
