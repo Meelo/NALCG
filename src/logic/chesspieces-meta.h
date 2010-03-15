4afe4f175145fd93f27ab54dbd4250c4287c010a
Niko Hämäläinen <milo@meelo.org>
2010-03-15 19:32:36 +0200

    meta-include added for chesspieces..

---
+++ src/logic/chesspieces-meta.h
@@ -0,0 +1,12 @@
+#ifndef _NALCG_CHESS_PIECES_META_H_
+#define _NALCG_CHESS_PIECES_META_H_
+
+#include "../../src/logic/piece.h"
+#include "../../src/logic/pawn.h"
+#include "../../src/logic/rook.h"
+#include "../../src/logic/knight.h"
+#include "../../src/logic/bishop.h"
+#include "../../src/logic/queen.h"
+#include "../../src/logic/king.h"
+
+#endif // _NALCG_CHESS_PIECES_META_H_
