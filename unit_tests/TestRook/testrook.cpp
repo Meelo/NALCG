feb12474d07c537108ca28254cc10816c1a7c2a5
Niko Hämäläinen <milo@meelo.org>
2010-03-20 21:15:01 +0200

    black rook movement tested and implemented

--- unit_tests/TestRook/testrook.cpp
+++ unit_tests/TestRook/testrook.cpp
@@ -168,14 +168,84 @@ void TestRook::blackValidMoves_data()
     // These will be handled as instructions for creating new units
     // before running the actual tests.
     PieceHolder createUnitsAt01[] = { };
+    PieceHolder createUnitsAt02[] = { PieceHolder(15, 0) };
+    PieceHolder createUnitsAt03[] = { PieceHolder(6, 0) };
+    PieceHolder createUnitsAt04[] = {   PieceHolder(8, 0),
+                                        PieceHolder(24, new Pawn(Piece::BLACK)) };
+    PieceHolder createUnitsAt05[] = {   PieceHolder(51, new Rook(Piece::BLACK)),
+                                        PieceHolder(43, new Pawn(Piece::WHITE)) };
+    PieceHolder createUnitsAt06[] = {   PieceHolder(33, new Rook(Piece::BLACK)),
+                                        PieceHolder(9,  new Pawn(Piece::WHITE)) };
+    PieceHolder createUnitsAt07[] = {   PieceHolder(34, new Rook(Piece::BLACK)),
+                                        PieceHolder(42, new Pawn(Piece::BLACK)),
+                                        PieceHolder(26, new Pawn(Piece::BLACK)),
+                                        PieceHolder(38, new Pawn(Piece::BLACK)) };
+    PieceHolder createUnitsAt08[] = {   PieceHolder(29, new Rook(Piece::BLACK)),
+                                        PieceHolder(21, new Pawn(Piece::BLACK)),
+                                        PieceHolder(28, new Pawn(Piece::BLACK)),
+                                        PieceHolder(30, new Pawn(Piece::BLACK)),
+                                        PieceHolder(37, new Pawn(Piece::BLACK)) };
+    PieceHolder createUnitsAt09[] = {   PieceHolder(29, new Rook(Piece::BLACK)),
+                                        PieceHolder(21, new Pawn(Piece::WHITE)),
+                                        PieceHolder(28, new Pawn(Piece::WHITE)),
+                                        PieceHolder(30, new Pawn(Piece::WHITE)),
+                                        PieceHolder(37, new Pawn(Piece::WHITE)) };
 
     // These are the expected valid move indices.
     std::size_t case01[] = { };
+    std::size_t case02[] = { 15, 23, 31, 39, 47, 55 };
+    std::size_t case03[] = { 6 };
+    std::size_t case04[] = { 8, 16 };
+    std::size_t case05[] = { 43, 50, 52, 59 };
+    std::size_t case06[] = { 9, 17, 25, 32, 34, 35, 36, 37, 38, 39, 41, 49 };
+    std::size_t case07[] = { 32, 33, 35, 36, 37 };
+    std::size_t case08[] = { };
+    std::size_t case09[] = { 21, 28, 30, 37 };
 
     QTest::newRow("starting positions, a8") << std::size_t(0) 
         << std::vector<PieceHolder>
             (createUnitsAt01, createUnitsAt01 + sizeof(createUnitsAt01) / sizeph)
         << std::vector<std::size_t>(case01, case01 + sizeof(case01) / sizet);
+
+    QTest::newRow("starting positiions, no pawn at h7") << std::size_t(7) 
+        << std::vector<PieceHolder>
+            (createUnitsAt02, createUnitsAt02 + sizeof(createUnitsAt02) / sizeph)
+        << std::vector<std::size_t>(case02, case02 + sizeof(case02) / sizet);
+
+    QTest::newRow("starting positions, no  knight at g8") << std::size_t(7) 
+        << std::vector<PieceHolder>
+            (createUnitsAt03, createUnitsAt03 + sizeof(createUnitsAt03) / sizeph)
+        << std::vector<std::size_t>(case03, case03 + sizeof(case03) / sizet);
+
+    QTest::newRow("starting positions, pawn moved to a5") << std::size_t(0) 
+        << std::vector<PieceHolder>
+            (createUnitsAt04, createUnitsAt04 + sizeof(createUnitsAt04) / sizeph)
+        << std::vector<std::size_t>(case04, case04 + sizeof(case04) / sizet);
+
+    QTest::newRow("rook ate d2") << std::size_t(51) 
+        << std::vector<PieceHolder>
+            (createUnitsAt05, createUnitsAt05 + sizeof(createUnitsAt05) / sizeph)
+        << std::vector<std::size_t>(case05, case05 + sizeof(case05) / sizet);
+
+    QTest::newRow("movement all around") << std::size_t(33) 
+        << std::vector<PieceHolder>
+            (createUnitsAt06, createUnitsAt06 + sizeof(createUnitsAt06) / sizeph)
+        << std::vector<std::size_t>(case06, case06 + sizeof(case06) / sizet);
+
+    QTest::newRow("only sideways movement allowed") << std::size_t(34) 
+        << std::vector<PieceHolder>
+            (createUnitsAt07, createUnitsAt07 + sizeof(createUnitsAt07) / sizeph)
+        << std::vector<std::size_t>(case07, case07 + sizeof(case07) / sizet);
+
+    QTest::newRow("blocked by allies, middle of board") << std::size_t(29) 
+        << std::vector<PieceHolder>
+            (createUnitsAt08, createUnitsAt08 + sizeof(createUnitsAt08) / sizeph)
+        << std::vector<std::size_t>(case08, case08 + sizeof(case08) / sizet);
+
+    QTest::newRow("surrounded by baddies, middle of board") << std::size_t(29) 
+        << std::vector<PieceHolder>
+            (createUnitsAt09, createUnitsAt09 + sizeof(createUnitsAt09) / sizeph)
+        << std::vector<std::size_t>(case09, case09 + sizeof(case09) / sizet);
 }
 
 void TestRook::blackValidMoves()
