6416d4d90ced47054b5471ff53d6cca905050818
Niko Hämäläinen <milo@meelo.org>
2010-03-21 00:28:30 +0200

    knight movement has now been tested and implemented

--- unit_tests/TestKnight/testknight.cpp
+++ unit_tests/TestKnight/testknight.cpp
@@ -51,31 +51,31 @@ void TestKnight::whiteValidMoves_data()
     // before running the actual tests.
     PieceHolder createUnitsAt01[] = { };
     PieceHolder createUnitsAt02[] = { };
-    PieceHolder createUnitsAt03[] = { PieceHolder(45, new Pawn(Piece::WHITE)) };
-    PieceHolder createUnitsAt04[] = { PieceHolder(42, new Pawn(Piece::WHITE)) };
+    PieceHolder createUnitsAt03[] = { PieceHolder(51, 0) };
+    PieceHolder createUnitsAt04[] = { PieceHolder(52, 0) };
 
     // These are the expected valid move indices.
-    std::size_t case01[] = { 45, 47 };
-    std::size_t case02[] = { 40, 42 };
-    std::size_t case03[] = { 47 };
-    std::size_t case04[] = { 40 };
+    std::size_t case01[] = { 40, 42 };
+    std::size_t case02[] = { 45, 47 };
+    std::size_t case03[] = { 40, 42, 51 };
+    std::size_t case04[] = { 45, 47, 52 };
 
-    QTest::newRow("starting positions, g1") << std::size_t(62)
+    QTest::newRow("starting positions, b1") << std::size_t(57)
         << std::vector<PieceHolder>
             (createUnitsAt01, createUnitsAt01 + sizeof(createUnitsAt01) / sizeph)
         << std::vector<std::size_t>(case01, case01 + sizeof(case01) / sizet);
 
-    QTest::newRow("starting positions, b1") << std::size_t(57)
+    QTest::newRow("starting positions, g1") << std::size_t(62)
         << std::vector<PieceHolder>
             (createUnitsAt02, createUnitsAt02 + sizeof(createUnitsAt02) / sizeph)
         << std::vector<std::size_t>(case02, case02 + sizeof(case02) / sizet);
    
-    QTest::newRow("starting positions, g1, pawn at f3") << std::size_t(62)
+    QTest::newRow("starting positions, b1, empty at d2") << std::size_t(57)
         << std::vector<PieceHolder>
             (createUnitsAt03, createUnitsAt03 + sizeof(createUnitsAt03) / sizeph)
         << std::vector<std::size_t>(case03, case03 + sizeof(case03) / sizet);
    
-    QTest::newRow("starting positions, g1, pawn at c3") << std::size_t(57)
+    QTest::newRow("starting positions, g1, empty at e2") << std::size_t(62)
         << std::vector<PieceHolder>
             (createUnitsAt04, createUnitsAt04 + sizeof(createUnitsAt04) / sizeph)
         << std::vector<std::size_t>(case04, case04 + sizeof(case04) / sizet);
@@ -144,11 +144,13 @@ void TestKnight::blackValidMoves_data()
     PieceHolder createUnitsAt01[] = { };
     PieceHolder createUnitsAt02[] = { };
     PieceHolder createUnitsAt03[] = { PieceHolder(11, 0) };
+    PieceHolder createUnitsAt04[] = { PieceHolder(12, 0) };
 
     // These are the expected valid move indices.
     std::size_t case01[] = { 16, 18};
     std::size_t case02[] = { 21, 23 };
-    std::size_t case03[] = { 11, 21, 23 };
+    std::size_t case03[] = { 11, 16, 18 };
+    std::size_t case04[] = { 12, 21, 23 };
 
     QTest::newRow("starting positions, b8") << std::size_t(1) 
         << std::vector<PieceHolder>
@@ -160,11 +162,15 @@ void TestKnight::blackValidMoves_data()
             (createUnitsAt02, createUnitsAt02 + sizeof(createUnitsAt02) / sizeph)
         << std::vector<std::size_t>(case02, case02 + sizeof(case02) / sizet);
     
-    QTest::newRow("starting positions, b8, empty d7") << std::size_t(2) 
+    QTest::newRow("starting positions, b8, empty d7") << std::size_t(1) 
         << std::vector<PieceHolder>
             (createUnitsAt03, createUnitsAt03 + sizeof(createUnitsAt03) / sizeph)
         << std::vector<std::size_t>(case03, case03 + sizeof(case03) / sizet);
 
+    QTest::newRow("starting positions, g8, empty e7") << std::size_t(6) 
+        << std::vector<PieceHolder>
+            (createUnitsAt04, createUnitsAt04 + sizeof(createUnitsAt04) / sizeph)
+        << std::vector<std::size_t>(case04, case04 + sizeof(case04) / sizet);
 
 }
 
@@ -187,8 +193,14 @@ void TestKnight::blackValidMoves()
     Board* board = new ChessBoard(squares);
     
     std::vector<std::size_t> knightMoves = board->getValidMoves(unit_location);
-
-
+    static int counterB = 0;
+    ++counterB;
+    std::cout << "--- " << counterB << " ---" << std::endl;
+    for (std::size_t i = 0; i < knightMoves.size(); ++i)
+    {
+        std::cout << knightMoves.at(i) << " ";
+    }
+    std::cout << std::endl << std::endl;
     QCOMPARE(knightMoves.size(), valid_moves.size());
 
     for (std::size_t i = 0; i < valid_moves.size(); ++i)
