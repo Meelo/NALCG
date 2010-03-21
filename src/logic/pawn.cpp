132997eecf8060b6bce0cd9e3cebfef5eecb2ff8
Niko Hämäläinen <milo@meelo.org>
2010-03-21 16:00:29 +0200

    bug with pawn movement fixed

--- src/logic/pawn.cpp
+++ src/logic/pawn.cpp
@@ -4,14 +4,13 @@
 #include "pawn.h"
 #include "chessboard.h"
 
-Pawn::Pawn(const Piece::Colour& colour) : Piece(colour, "Pawn"),
-    DIAGONAL_MOVE_LEFT(ChessBoard::WIDTH + 1), 
-    DIAGONAL_MOVE_RIGHT(ChessBoard::WIDTH - 1)
+Pawn::Pawn(const Piece::Colour& colour) : Piece(colour, "Pawn")
 {
 
 }
 
-std::vector<std::size_t> Pawn::getValidMoves(std::size_t location, const std::vector<Square>& squares) const
+std::vector<std::size_t> Pawn::getValidMoves(std::size_t location, 
+    const std::vector<Square>& squares) const
 {
     std::vector<std::size_t> validMoves;
 
@@ -20,9 +19,10 @@ std::vector<std::size_t> Pawn::getValidMoves(std::size_t location, const std::ve
     else if (colour == Piece::BLACK) direction = 1;
     else return validMoves;
 
-    std::size_t y = location / ChessBoard::HEIGHT;
-    std::size_t diagL = location + (direction * DIAGONAL_MOVE_LEFT);
-    std::size_t diagR = location + (direction * DIAGONAL_MOVE_RIGHT);
+    std::size_t x = 0, y = 0;
+    ChessBoard::getCoordinates(location, x, y);
+    std::size_t diagL = ChessBoard::getPosition(x - 1, y + direction);
+    std::size_t diagR = ChessBoard::getPosition(x + 1, y + direction);
 
     if (diagL < squares.size() && 
         isOppositeColour(squares.at(diagL).getColourOfPiece()))
@@ -36,15 +36,17 @@ std::vector<std::size_t> Pawn::getValidMoves(std::size_t location, const std::ve
         validMoves.push_back(diagR);
     }
 
-    std::size_t possibility = location + (direction * ChessBoard::WIDTH);
+    std::size_t possibility = ChessBoard::getPosition(x, y + direction);
     if (possibility < squares.size() && !squares.at(possibility).hasPiece())
     {
         validMoves.push_back(possibility);
         
-        if ((colour == Piece::WHITE && y == ChessBoard::WHITE_PAWN_ROW) || (colour == Piece::BLACK && y == ChessBoard::BLACK_PAWN_ROW))
+        if ((colour == Piece::WHITE && y == ChessBoard::WHITE_PAWN_ROW) || 
+            (colour == Piece::BLACK && y == ChessBoard::BLACK_PAWN_ROW))
         {
             possibility += (direction * ChessBoard::WIDTH);
-            if (possibility < squares.size() && !squares.at(possibility).hasPiece())
+            if (possibility < squares.size() &&
+                !squares.at(possibility).hasPiece())
             {
                 validMoves.push_back(possibility);
             }
