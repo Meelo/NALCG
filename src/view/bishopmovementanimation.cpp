8785b69025dbb0033439da7bbf3c555323cfed8a
Markku Velinen <dzarkki@hotmail.com>
2010-03-23 09:34:11 +0200

    Added flame effect that still crashes the game.

--- src/view/bishopmovementanimation.cpp
+++ src/view/bishopmovementanimation.cpp
@@ -1,23 +1,51 @@
 #include "bishopmovementanimation.h"
 
 bool BishopMovementAnimation::animate(const Real& timeSinceLastFrame)
-    {
-        Real distanceMoved = MOVEMENT_SPEED * timeSinceLastFrame;
-        Vector3 path = mDestination - mAnimatedNode->getPosition();
+{
+    Real distanceMoved = MOVEMENT_SPEED * timeSinceLastFrame;
+    Vector3 path = mDestination - mAnimatedNode->getPosition();
 
-        if (path.length() > distanceMoved)
+    if (path.length() > distanceMoved)
+    {
+        if (path.length() < 100 && mAttackDuration > 0)
         {
+            if (!mParticleNode)
+            {
+                createBlasts();
+            }
+            mAttackDuration -= timeSinceLastFrame;
+        }
+        else {
+            if (mParticleNode)
+            {
+                mAnimatedNode->removeAndDestroyChild(mParticleNode->getName());
+                mParticleNode = 0;
+            }
             // Normalising the vector so the speed remains constant.
             path.normalise();
             mAnimatedNode->translate(path * distanceMoved);
 
             Vector3 src = mAnimatedNode->getOrientation() * Vector3::UNIT_Z;
             mAnimatedNode->rotate(src.getRotationTo(path));
-            return true; // Animation still running.
         }
-
-        mAnimatedNode->setPosition(mDestination);
-        mAnimatedNode->setOrientation(mAnimatedNode->getInitialOrientation());
-        return false; // Animation finished.
+        return true; // Animation still running.
     }
 
+    mAnimatedNode->setPosition(mDestination);
+    mAnimatedNode->setOrientation(mAnimatedNode->getInitialOrientation());
+    return false; // Animation finished.
+}
+
+void BishopMovementAnimation::createBlasts()
+{
+    // Create a bloodstorm
+    ParticleSystem* pSys = mSceneMgr->createParticleSystem(
+        nextName(), "Effects/Flame");
+    mParticleNode = mAnimatedNode->createChildSceneNode();
+    //const Vector3& position = pieceNode->getPosition();
+    mParticleNode->translate(0, 150, 0);
+    mParticleNode->attachObject(pSys);
+    ParticleEmitter *emitter = pSys->getEmitter(0);
+    emitter->setTimeToLive(mAttackDuration + 1);
+    pSys->setParticleQuota(emitter->getEmissionRate() * mAttackDuration);
+}
--- src/view/bishopmovementanimation.h
+++ src/view/bishopmovementanimation.h
@@ -7,12 +7,16 @@ class BishopMovementAnimation : public MovementAnimation
 public:
     BishopMovementAnimation(const Vector3& destination, SceneNode *movingNode,
         SceneNode *targetPiece, SceneManager *sceneMgr, AnimationManager *animationManager)
-        : MovementAnimation(destination, movingNode, targetPiece, sceneMgr, animationManager)
+        : MovementAnimation(destination, movingNode, targetPiece, sceneMgr, animationManager),
+        mAttackDuration(targetPiece ? 5.0 : -1), mParticleNode(0)
     {
     }
 
     virtual bool animate(const Real& timeSinceLastFrame);
+    virtual void createBlasts();
 protected:
     static const int MOVEMENT_SPEED = 500;
+    Real mAttackDuration;
+    SceneNode *mParticleNode;
 };
 
