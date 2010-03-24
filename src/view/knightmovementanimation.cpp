288b86c181dd687742584009bca7bc6eff04a7ee
Markku Velinen <dzarkki@hotmail.com>
2010-03-24 20:35:32 +0200

    Basic knight jumping animation done.

--- src/view/knightmovementanimation.cpp
+++ src/view/knightmovementanimation.cpp
@@ -4,16 +4,22 @@ bool KnightMovementAnimation::animate(const Real& timeSinceLastFrame)
 {
     Real distanceMoved = MOVEMENT_SPEED * timeSinceLastFrame;
     Vector3 path = mDestination - mAnimatedNode->getPosition();
+    path.y = 0;
 
     if (path.length() > distanceMoved)
     {
+        // Initial vertical velocity is half of the length of the total distance
+        // Path shortens while knight travels towards its target and has the same
+        // effect as gravity would have to the vertical speed.
+        Real verticalVelocity = (path.length() - mHalfTotalDistance) * JUMP_HEIGHT;
+        mAnimatedNode->translate(Vector3(0, verticalVelocity * timeSinceLastFrame, 0)); 
 
         // Normalising the vector so the speed remains constant.
         path.normalise();
         mAnimatedNode->translate(path * distanceMoved);
 
-        Vector3 src = mAnimatedNode->getOrientation() * Vector3::UNIT_Z;
-        mAnimatedNode->rotate(src.getRotationTo(path));
+        //Vector3 src = mAnimatedNode->getOrientation() * Vector3::UNIT_Z;
+        //mAnimatedNode->rotate(src.getRotationTo(path));
         return true; // Animation still running.
     }
 
--- src/view/knightmovementanimation.h
+++ src/view/knightmovementanimation.h
@@ -7,12 +7,15 @@ class KnightMovementAnimation : public MovementAnimation
 public:
     KnightMovementAnimation(const Vector3& destination, SceneNode *movingNode,
         SceneNode *targetPiece, SceneManager *sceneMgr, AnimationManager *animationManager)
-        : MovementAnimation(destination, movingNode, targetPiece, sceneMgr, animationManager)
+        : MovementAnimation(destination, movingNode, targetPiece, sceneMgr, animationManager),
+        mHalfTotalDistance((mDestination - mAnimatedNode->getPosition()).length() / 2)
     {
     }
 
     virtual bool animate(const Real& timeSinceLastFrame);
 protected:
     static const int MOVEMENT_SPEED = 500;
+    static const int JUMP_HEIGHT = 10;
+    const Real mHalfTotalDistance;
 };
 
