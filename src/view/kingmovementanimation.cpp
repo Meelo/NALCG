e71ddcd4c374b9e6ee14750c8120d5feb4bf8c5d
Markku Velinen <dzarkki@hotmail.com>
2010-04-03 17:25:38 +0300

    King now has an attack animation too.

--- src/view/kingmovementanimation.cpp
+++ src/view/kingmovementanimation.cpp
@@ -6,43 +6,78 @@ bool KingMovementAnimation::animate(const Real& timeSinceLastFrame)
 {
     Real distanceMoved = MOVEMENT_SPEED * timeSinceLastFrame;
     Vector3 path = mDestination - mAnimatedNode->getPosition();
+    path.y = 0;
+    Real verticalVelocity = (path.length() - mHalfTotalDistance) * mFlyingAltitude;
 
     if (path.length() > distanceMoved || mParticleNode)
     {
-        if (path.length() < 280 && mAttackDuration > 0)
+        switch (mPhase)
         {
-            if (!mParticleNode)
-            {
-                createBlasts();
-                mAnimationManager->addAnimation(
-                    AnimationFactory::createDyingAnimation(
-                    mTargetPiece, mSceneMgr, 0.5, 2));
-                mAnimationManager->addAnimation(
-                    AnimationFactory::createBleedingAnimation(
-                    mTargetPiece, mSceneMgr, 0.3, 2, "Effects/Burn"));
-                mAnimationManager->addAnimation(
-                    AnimationFactory::createBleedingAnimation(
-                    mTargetPiece, mSceneMgr, 0.3, 2.5, "Effects/Smoke"));
-            }
-            if (mAttackDuration < 2.5 && mSceneMgr->hasSceneNode(mTargetPieceName))
-            {
-                mTargetPiece->yaw(Degree(timeSinceLastFrame * 140));
-            }
+            case 1:
+                if (verticalVelocity <= 0)
+                {
+                    mPhase = 2;
+                }
+                break;
+            case 2:
+                if (mAttackDuration >= 0)
+                {
+                    if (!mParticleNode)
+                    {
+                        createBlasts();
+                        mAnimationManager->addAnimation(
+                            AnimationFactory::createDyingAnimation(
+                            mTargetPiece, mSceneMgr, 2, 3));
+                        mAnimationManager->addAnimation(
+                            AnimationFactory::createBleedingAnimation(
+                            mTargetPiece, mSceneMgr, 0.2, 2.5));
+                        mAnimationManager->addAnimation(
+                            AnimationFactory::createBleedingAnimation(
+                            mTargetPiece, mSceneMgr, 0.2, 2, "Effects/Smoke"));
+                    }
 
-            mAttackDuration -= timeSinceLastFrame;
+                    distanceMoved = 0;
+                    mAttackDuration -= timeSinceLastFrame;
+                }
+                else
+                {
+                    mPhase = 3;
+                    if (mParticleNode)
+                    {
+                        mAnimatedNode->removeAndDestroyChild(mParticleNode->getName());
+                        mParticleNode = 0;
+                    }
+                }
+                break;
         }
-        else {
-            if (mParticleNode)
-            {
-                mAnimatedNode->removeAndDestroyChild(mParticleNode->getName());
-                mParticleNode = 0;
-            }
+
+
+        if (distanceMoved > 0)
+        {
+            mAnimatedNode->translate(Vector3(0, verticalVelocity * timeSinceLastFrame, 0)); 
+
+            Vector3 realPath = path;
+
             // Normalising the vector so the speed remains constant.
             path.normalise();
             mAnimatedNode->translate(path * distanceMoved);
 
-            Vector3 src = mAnimatedNode->getOrientation() * Vector3::UNIT_Z;
+            mAnimatedNode->resetOrientation();
+            Vector3 src = Vector3::UNIT_Z;
             mAnimatedNode->rotate(src.getRotationTo(path));
+            
+            if (mTargetPiece)
+            {
+                Real pitch = realPath.length() * 45 / mHalfTotalDistance;
+                if (mPhase == 1)
+                {
+                    pitch = 90 - pitch;
+                }
+                mAnimatedNode->pitch(Degree(pitch));
+            }
+
+            mAnimatedNode->yaw(Degree(realPath.length() * 360 / mHalfTotalDistance));
+
         }
         return true; // Animation still running.
     }
@@ -56,10 +91,10 @@ void KingMovementAnimation::createBlasts()
 {
     // Create a bloodstorm
     ParticleSystem* pSys = mSceneMgr->createParticleSystem(
-        nextName(), "Effects/Flame");
+        nextName(), "Effects/EnergyBlast");
     mParticleNode = mAnimatedNode->createChildSceneNode();
     //const Vector3& position = pieceNode->getPosition();
-    mParticleNode->translate(-90, 270, 90);
+    mParticleNode->translate(0, 160, 60);
     mParticleNode->attachObject(pSys);
     ParticleEmitter *emitter = pSys->getEmitter(0);
     //emitter->setTimeToLive(mAttackDuration + 1);
--- src/view/kingmovementanimation.h
+++ src/view/kingmovementanimation.h
@@ -9,16 +9,21 @@ public:
     KingMovementAnimation(const Vector3& destination, SceneNode *movingNode,
         SceneNode *targetPiece, SceneManager *sceneMgr, AnimationManager *animationManager)
         : MovementAnimation(destination, movingNode, targetPiece, sceneMgr, animationManager),
-        mAttackDuration(targetPiece ? 3.0 : -1), mParticleNode(0)
+        mAttackDuration(targetPiece ? 3.0 : -1), mParticleNode(0),
+        mPhase(1), mHalfTotalDistance((mDestination - mAnimatedNode->getPosition()).length() / 2),
+        mFlyingAltitude(2400.0 / mHalfTotalDistance - 9)
     {
     }
 
     virtual bool animate(const Real& timeSinceLastFrame);
     virtual void createBlasts();
 protected:
-    static const int MOVEMENT_SPEED = 500;
+    static const int MOVEMENT_SPEED = 200;
     Real mAttackDuration;
     SceneNode *mParticleNode;
+    int mPhase;
+    const Real mHalfTotalDistance;
+    const int mFlyingAltitude;
 };
 
 #endif // _NALCG_KING_MOVEMENT_ANIMATION_H_
