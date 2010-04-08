3c71c7005c0308532883e18d94eb89cb8a429bd3
Markku Velinen <dzarkki@hotmail.com>
2010-04-09 00:53:09 +0300

    Pawn promotion is now animated.

--- src/view/promotionanimation.cpp
+++ src/view/promotionanimation.cpp
@@ -5,13 +5,48 @@
 
 bool PromotionAnimation::animate(const Real& timeSinceLastFrame)
 {
-    char upperCaseSymbol = promotionToSymbol(mPromoteTo);
-    char symbol = upperCaseSymbol;
-    if (!mView->isWhiteTurn())
+    if (mPromotionDuration >= 0 || !transformed)
     {
-        symbol |= 1 << 5;
+        if (!mParticleNode && !transformed)
+        {
+            createBlasts();
+        }
+        
+        Vector3 position = mAnimatedNode->getPosition();
+        position.y = sin((mPromotionDuration - 1.5) * Math::PI / (PROMOTION_DURATION - 1.5)) * 150;
+        mAnimatedNode->setPosition(position);
+        mAnimatedNode->yaw(Degree(timeSinceLastFrame * 360 / (PROMOTION_DURATION - 1.5)));
+
+        if (!mWhiteFlashNode && mPromotionDuration < 3)
+        {
+            createWhiteFlash();
+        }
+        if (!transformed && mPromotionDuration < 1.5)
+        {
+            char upperCaseSymbol = promotionToSymbol(mPromoteTo);
+            char symbol = upperCaseSymbol;
+            if (!mView->isWhiteTurn())
+            {
+                symbol |= 1 << 5;
+            }
+            position.y = 0;
+            mView->createPiece(upperCaseSymbol, mView->getMeshName(symbol), position);
+
+            mAnimatedNode->scale(0.5, 0.5, 0.5);
+
+            mAnimatedNode->removeAndDestroyChild(mParticleNode->getName());
+            mParticleNode = 0;
+
+            transformed = true;
+        }
+
+
+        mPromotionDuration -= timeSinceLastFrame;
+        return true;
     }
-    mView->createPiece(upperCaseSymbol, mView->getMeshName(symbol), mAnimatedNode->getPosition());
+
+    mAnimatedNode->removeAndDestroyChild(mWhiteFlashNode->getName());
+    mWhiteFlashNode = 0;
 
     mSceneMgr->getRootSceneNode()->removeAndDestroyChild(mAnimatedNode->getName());
     return false;
@@ -24,4 +59,32 @@ char PromotionAnimation::promotionToSymbol(unsigned int promoteTo) const
     if (promoteTo == ChessBoard::PROMOTE_TO_ROOK) return 'R';
     if (promoteTo == ChessBoard::PROMOTE_TO_BISHOP) return 'B';
     return 0;
+}
+
+void PromotionAnimation::createBlasts()
+{
+    ParticleSystem* pSys = mSceneMgr->createParticleSystem(
+        nextName(), "Effects/Aureola");
+    mParticleNode = mAnimatedNode->createChildSceneNode();
+    //const Vector3& position = pieceNode->getPosition();
+    mParticleNode->translate(0, 0, 0);
+    mParticleNode->attachObject(pSys);
+    ParticleEmitter *emitter = pSys->getEmitter(0);
+
+    pSys->fastForward(0.5);
+    //pSys->setParticleQuota(emitter->getEmissionRate() * mPromotionDuration);
+}
+
+
+void PromotionAnimation::createWhiteFlash()
+{
+    ParticleSystem* pSys = mSceneMgr->createParticleSystem(
+        nextName(), "Effects/WhiteFlash");
+    mWhiteFlashNode = mAnimatedNode->createChildSceneNode();
+    //const Vector3& position = pieceNode->getPosition();
+    mWhiteFlashNode->translate(0, 150, 0);
+    mWhiteFlashNode->attachObject(pSys);
+    ParticleEmitter *emitter = pSys->getEmitter(0);
+
+    //pSys->setParticleQuota(emitter->getEmissionRate());
 }
