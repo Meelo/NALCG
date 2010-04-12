9a38c5e065929072bcdaba76a0712546caaccf81
Markku Velinen <dzarkki@hotmail.com>
2010-04-12 22:53:35 +0300

    King now jumps 70% higher when doing the castling move.

--- src/view/kingmovementanimation.h
+++ src/view/kingmovementanimation.h
@@ -9,9 +9,12 @@ public:
     KingMovementAnimation(const Vector3& destination, SceneNode *movingNode,
         SceneNode *targetPiece, SceneManager *sceneMgr, AnimationManager *animationManager)
         : MovementAnimation(destination, movingNode, targetPiece, sceneMgr, animationManager),
-        mAttackDuration(targetPiece ? 3.0 : -1), mParticleNode(0),
-        mPhase(1), mHalfTotalDistance((mDestination - mAnimatedNode->getPosition()).length() / 2),
-        mFlyingAltitude(2400.0 / mHalfTotalDistance - 9)
+        mAttackDuration(targetPiece ? 3.0 : -1), mParticleNode(0), mPhase(1),
+        mHalfTotalDistance((mDestination - mAnimatedNode->getPosition()).length() / 2),
+        // Ugly empirically found values to have the flying altitude be about
+        // the same in diagonal jumps too. Also it should be 70% higher when
+        // doing the castling move.
+        mFlyingAltitude((2400.0 / mHalfTotalDistance - 9) * (mHalfTotalDistance >= 150 ? 1.7 : 1))
     {
     }
 
