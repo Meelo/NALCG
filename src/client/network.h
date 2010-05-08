40fcf2386f1490a2eb1c69a3b12a3871b3089db4
Markku Velinen <dzarkki@hotmail.com>
2010-05-08 18:30:24 +0300

    View no longer runs in a separate thread from middleman. Network class is now privately implemented to allow shorter compiling times.

--- src/client/network.h
+++ src/client/network.h
@@ -3,31 +3,20 @@
 
 // system includes
 #include <string>
-#include <vector>
-
-// boost includes
-#include <boost/asio.hpp>
-#include <boost/thread/thread.hpp>
-
-// class dependencies
 
 class Network
 {
-    boost::asio::io_service io_service;
-    boost::asio::ip::tcp::socket socket;
-    boost::mutex mutex;
-    std::vector<std::string> linesBuffer;
-    char delimiter;
-    virtual void readToBuffer();
 public:
-    Network();
-    ~Network();
-    virtual bool connect(const char* ip, const char* port);
-    virtual void send(const std::string& message);
-    virtual void sendln(const std::string& message);
-    virtual void startBuffering(char delimiter = '\n');
-    virtual bool hasLines();
-    virtual std::string popLine();
+    virtual ~Network() { }
+
+    virtual bool connect(const char* ip, const char* port) = 0;
+    virtual void send(const std::string& message) = 0;
+    virtual void sendln(const std::string& message) = 0;
+    virtual void startBuffering(char delimiter = '\n') = 0;
+    virtual bool hasLines() = 0;
+    virtual std::string popLine() = 0;
+
+    static Network* createNewNetwork();
 };
 
 #endif // _NALCG_NETWORK_H_
